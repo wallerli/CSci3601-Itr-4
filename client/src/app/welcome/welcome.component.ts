a26b819d9f480f38d7905160212c8962daedf91d
wallerli <lsw626313@gmail.com>
2019-12-05 16:36:00 -0600

    Create the welcome page and add navigation functions #1 #15 #16 #17

---
+++ client/src/app/welcome/welcome.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {HomeService} from '../home/home.service';
+import {Room} from '../home/room';
+import {Observable} from 'rxjs';
+import {CookieService} from 'ngx-cookie-service';
+import {Router} from '@angular/router';
+
+@Component({
+  templateUrl: 'welcome.component.html',
+  // styleUrls: ['./welcome.component.css']
+})
+
+export class WelcomeComponent implements OnInit {
+
+  public rooms: Room[];
+
+  constructor(public homeService: HomeService, private cookieService: CookieService, private router: Router) {}
+
+  loadAllRooms(): void {
+    const rooms: Observable<Room[]> = this.homeService.getRooms();
+    rooms.subscribe(
+      // tslint:disable-next-line:no-shadowed-variable
+      rooms => {
+        this.rooms = rooms;
+      },
+      err => {
+        console.log(err);
+      });
+  }
+
+  ngOnInit(): void {
+    (async () => {
+      this.loadAllRooms();
+      if (this.router.url !== '/welcome' && this.cookieService.get('room_name') !== ''
+        && this.cookieService.get('room_name') !== 'All Rooms') {
+        await this.router.navigateByUrl('/home');
+      }
+    }) ();
+  }
+
+  async navigateTo(id: string) {
+    await this.router.navigateByUrl('/home/' + id);
+  }
+}
